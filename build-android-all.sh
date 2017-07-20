e2d649cb2ff7d9dbc28ee3beb100fbab820429ef
Nicolas Noury <noury@archos.com>
2017-07-20 10:37:31 +0200

    no more armeabi

--- build-android-all.sh
+++ build-android-all.sh
@@ -3,14 +3,14 @@
 set -e
 
 found=1
-for i in armeabi x86 armeabi-v7a;do
+for i in x86 armeabi-v7a;do
 	[ ! -f build-$i/lib/libboost_system-gcc-mt-1_55.a ] && found=0
 done
 
 [ "$found" == 1 ] && exit 0
 
 
-for i in armeabi x86 armeabi-v7a;do
+for i in x86 armeabi-v7a;do
 	[ -d boost_1_55_0-$i ] && mv boost_1_55_0-$i boost_1_55_0
 	bash build-android.sh --boost=1.55.0 --arch="$i" --without-libraries=python "$android_ndk"
 	mv boost_1_55_0 boost_1_55_0-$i
